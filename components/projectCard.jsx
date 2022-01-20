5a3679fa5ba06a1b7b0440f75d7b293e40304cc0
Thot11 <marc.ott@hetic.net>
2022-01-20 15:17:47 +0100

    first commit

---
+++ components/projectCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProjectCard = ({project}) => {
+
+  console.log(project)
+  
+  return (
+    <div className="projectCardContainer">
+      <p>{project.name}</p>
+    </div>
+  );
+};
+
+export default ProjectCard;
